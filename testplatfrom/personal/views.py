10221cbb99b631ad40e540ac6534dd6f3e359b04
liou <122498205@qq.com>
2020-06-28 19:58:52 +0800

    login

--- testplatfrom/personal/views.py
+++ testplatfrom/personal/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    """登录首页"""
+    if request.method =="GET":
+        return render(request,"index.html")
+    else:
+        username = request.GET.get("username", "")
+        password = request.GET.get("password", "")
+        print(username, password)
+        if username == "" or password == "":
+            return render(request, "index.html", {"error": "密码或用户名为空"})
+
+
+def login_action(request):
+    """处理登录请求 """
--- testplatfrom/testplatfrom/urls.py
+++ testplatfrom/testplatfrom/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from testplatfrom.testplatfrom.
-
+from personal import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', admin.site.urls),
+    path('index/', views.index),
 ]
